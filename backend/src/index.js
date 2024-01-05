382f7660352b052d7e37c3d9ee2b764534e60e17
Adiking117 <www.aditya117@gmail.com>
2024-01-05 23:35:08 +0530

    database connection

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import app from './app.js'
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path : './.env'
+})
+
+const port = process.env.PORT || 5000
+
+app.get(port,(req,res)=>{
+    res.send("Server Running ")
+})
+
+connectDB().then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is running on ${port}`)
+    })
+}).catch((error)=>{
+    console.log("Connection error : ",error)
+})
